e3feb3942c76615010bfd85f9bc570b331dccdcb
eagle9518 <j007wo2@gmail.com>
2026-04-03 16:39:04 -0700

    Add SensorData message_type and update conductivity to reflect all new changes

--- sensor-modules/controller/core/message_types.py
+++ sensor-modules/controller/core/message_types.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone
 from typing import Any, Protocol
 from dataclasses import dataclass
 
+
 class SerializableMsg(Protocol):
     def to_dict(self) -> dict[str, Any]: ...
 
@@ -12,6 +13,12 @@ class SerializableMsg(Protocol):
 
 @dataclass(slots=True)
 class String:
+    """
+    Simple wrapper around a string for testing purposes.
+    
+    This is not intended for production use, but rather just to have a simple 
+    message type for testing the system.
+    """
     data: str
 
     def to_dict(self) -> dict[str, Any]:
@@ -22,37 +29,50 @@ class String:
         return cls(data=data["data"])   
 
 
-# @dataclass(slots=True)
-# class Time:
-#     """
-#     Canonical time representation for the system.
-#
-#     Stores time as Unix epoch nanoseconds (UTC).
-#     """
-#     ns: int
-#
-#     def to_dict(self) -> dict[str, Any]:
-#         return {"ns": int(self.ns)}
-#
-#     @classmethod
-#     def from_dict(cls, data: dict[str, Any]) -> "Time":
-#         return cls(ns=int(data["ns"]))
-#
-#     @staticmethod
-#     def now() -> "Time":
-#         return Time(ns=time.time_ns())
-#
-#     def to_seconds(self) -> float:
-#         return self.ns / 1e9
-#
-#     def to_datetime_utc(self) -> datetime:
-#         return datetime.fromtimestamp(self.ns / 1e9, tz=timezone.utc)
-#
-#     def __str__(self) -> str:
-#         # ISO Format: YYYY-MM-DDThh:mm:ss
-#         return self.to_datetime_utc().isoformat()
-#
-#
+@dataclass(slots=True)
+class Time:
+    """
+    Canonical time representation for the system.
+
+    Stores time as Unix epoch nanoseconds (UTC).
+    """
+    ns: int
+
+    def to_dict(self) -> dict[str, Any]:
+        return {"ns": int(self.ns)}
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> "Time":
+        return cls(ns=int(data["ns"]))
+
+    @staticmethod
+    def now() -> "Time":
+        return Time(ns=time.time_ns())
+
+    def to_seconds(self) -> float:
+        return self.ns / 1e9
+
+    def to_datetime_utc(self) -> datetime:
+        return datetime.fromtimestamp(self.ns / 1e9, tz=timezone.utc)
+
+    def __str__(self) -> str:
+        # ISO Format: YYYY-MM-DDThh:mm:ss
+        return self.to_datetime_utc().isoformat()
+
+
+@dataclass(slots=True)
+class SensorData:
+    data: float
+    timestamp: Time
+
+    def to_dict(self) -> dict[str, Any]:
+        return { "data": self.data, "timestamp": self.timestamp.to_dict() }
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> "SensorData":
+        return cls(data=data["data"], timestamp=Time.from_dict(data["timestamp"]))
+    
+     
 # @dataclass(slots=True)
 # class PDB_Command:
 #     pin: str
--- sensor-modules/controller/nodes/conductivity.py
+++ sensor-modules/controller/nodes/conductivity.py
@@ -1,52 +1,36 @@
-import asyncio
 import serial_asyncio
 
 from core.node import Node 
-from core.message_types import String
-from core.launch import spin
+from core.message_types import SensorData, Time
 
-# TODO: Put these in a dedicated config file or something
-# TODO: /dev/USB0 could change, may need a script to scan connected USBs
-DEFAULT_SERIAL_PORT = "/dev/ttyUSB1"
-DEFAULT_BAUDRATE = 9600
 
 class Conductivity(Node):
     
-    def __init__(self):
-        super().__init__("conductivity")
-        # TODO: Anything else we need to publish?
-        self.conductivity_publisher = self.create_publisher(String, 'conductivity') 
+    def __init__(self, config: dict):
+        super().__init__("conductivity", config)
+        
+        self.serial_port = self.config.get("serial_port", "/dev/ttyUSB1")
+        self.baudrate = self.config.get("baudrate", 9600)
+        exchange_key = self.config.get("publish_exchange", "conductivity")
+        self.exchange_name = self.exchanges.get(exchange_key, exchange_key)
+        
+        self.conductivity_publisher = self.create_publisher(SensorData, self.exchange_name) 
         self.create_task(self.publish_conductivity)
 
     async def publish_conductivity(self):
         reader, writer = await serial_asyncio.open_serial_connection(
-            url=DEFAULT_SERIAL_PORT,
-            baudrate=DEFAULT_BAUDRATE
+            url=self.serial_port,
+            baudrate=self.baudrate
         )
 
         try:
             while True:
-                line = await reader.readline()
-                line = line.decode(errors="ignore").strip()
-                msg = String(data=line)
-                self.conductivity_publisher.publish(msg, "conductivity")
-        except asyncio.CancelledError:
-            try:
-                await writer.drain()
-            except Exception:
-                pass
-            raise
+                raw_data = await reader.readline()
+                timestamp = Time.now()
+                data = float(raw_data.decode(errors="ignore").strip())
+                msg = SensorData(data=data, timestamp=timestamp)
+                self.conductivity_publisher.publish(msg, self.exchange_name)
         finally:
-            try:
-                writer.close()
-                await writer.wait_closed()
-            except Exception:
-                pass
-        
-def main(args=None):
-    conductivity = Conductivity()
-    spin(conductivity)
-
-if __name__ == '__main__':
-    main()
+            writer.close()
+            await writer.wait_closed()
 
