cdc0f3ee570358490b00bd35fa4c056378a74ec4
Logan <loganwong.lw@gmail.com>
2026-05-02 00:39:40 +0000

    Add basic heartbeat node

---
+++ sensor-modules/controller/nodes/heartbeat.py
@@ -0,0 +1,28 @@
+import asyncio
+from gpiozero import DigitalOutputDevice
+
+from core.node import Node 
+
+
+PULSE_PIN = 27 
+
+
+class Heartbeat(Node):
+    
+    def __init__(self, config: dict):
+        super().__init__("heartbeat", config)
+        
+        self.on_time = self.node_config.get("high_time", 1.0)
+        self.off_time = self.node_config.get("low_time", 1.0)
+        
+        self.pulse_pin = DigitalOutputDevice(PULSE_PIN)
+
+        self.create_task(self.send_pulses)
+
+    async def send_pulses(self):
+        while True:
+            self.pulse_pin.on()
+            await asyncio.sleep(self.on_time)
+            self.pulse_pin.off()
+            await asyncio.sleep(self.off_time)
+
